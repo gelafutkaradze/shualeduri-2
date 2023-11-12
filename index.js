0b9c724e78b19e8a1bb2a706642cabd4a7f6e94d
gela <gelafutkaradze04@gmail.com>
2023-11-12 21:50:39 +0400

    firstinit

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const age = 0;
+
+
+if (age >= 18) {
+    console.log('ასაკი მეტია ან ტოლი 18-ზე')
+  } else {
+    console.log('ასაკი ნაკლებია ან ტოლი 18-ზე')
+  }
+
+
+
+  const weekDay = 8;
+
+
+  switch (weekDay) {
+    case 1:
+      console.log("Sunday")
+      break;
+    case 2:
+      console.log("Monday")
+      break;
+    case 3:
+       console.log("Tuesday")
+      break;
+    case 4:
+      console.log("Wednesday")
+      break;
+    case 5:
+      console.log("Thursday")
+      break;
+    case 6:
+      console.log("Friday")
+      break;
+    case 7:
+      console.log("Saturday")
+      break;
+      default: console.log("Your weekDay is not correct")
+  }
+
